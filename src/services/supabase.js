ec4e9bb40948269dc0631e86cba9296134d51006
adilarain00 <addilarain00@gmail.com>
2025-05-07 17:10:54 +0500

    The Wild Aosis

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+export const supabaseUrl = "https://edyrvcvugwltlycnflxw.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeXJ2Y3Z1Z3dsdGx5Y25mbHh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MTYzMzUsImV4cCI6MjA2MjE5MjMzNX0.xHL-MFUTWOpdC0cwj4Q4naGwSXDuS96PYj08VRKIeT0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
