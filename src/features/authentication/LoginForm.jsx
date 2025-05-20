c397a4cf92b232fef2e27990e02870e716fee8d0
adilarain00 <addilarain00@gmail.com>
2025-05-20 23:35:49 +0500

    The Wild Aosis

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("example@gmail.com");
+  const [password, setPassword] = useState("example");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
